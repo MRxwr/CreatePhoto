ab27278aaaa354000b6c458828099245077b84f1
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-01-28 00:39:12 +0530

    success page

--- plugins/sbhadra/payapi/src/Http/Controllers/PayapiController.php
+++ plugins/sbhadra/payapi/src/Http/Controllers/PayapiController.php
@@ -96,6 +96,7 @@ class PayapiController extends FrontendController
         
         if(isset($_REQUEST['paymentId'])){
             $payment_data = Session::get('booking_data');
+            //dd($payment_data);
             $paymentId = $_REQUEST['paymentId'];
             $params = [
                 'endpoint' => 'PaymentStatusCheck',
@@ -104,9 +105,9 @@ class PayapiController extends FrontendController
                 'KeyType' => 'invoiceid'
             ];
             $curl = curl_init();
-            // $certificate_location = 'C:\wamp64\bin\php\php7.2.33\extras\ssl\cacert.pem';
-            // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $certificate_location);
-            // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $certificate_location);
+            $certificate_location = 'C:\wamp64\bin\php\php7.2.33\extras\ssl\cacert.pem';
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $certificate_location);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $certificate_location);
             curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://createkwservers.com/payapi/api/v2/index.php',
             CURLOPT_RETURNTRANSFER => true,
@@ -132,12 +133,15 @@ class PayapiController extends FrontendController
                 return redirect()->back()->withErrors(['msg' => 'The Message']);
             } else {
                 $res = json_decode($response);
+                dd($res);
                 if($res->type == 'success' && isset($res->data->InvoiceId)){
                     $booking = Booking::find($payment_data['booking_id']);
                     $booking->status =  'Yes';
                     if($booking->save()){
                        
                     }
+                }else{
+                    
                 }
             //$booking_data = Session::get("booking_data");
             //dd($booking_data);
