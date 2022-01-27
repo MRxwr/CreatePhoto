cf3a3a11c67413a46b49a879b28aa52fc06e6884
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-01-27 23:53:45 +0530

    Update app.blade.php

--- themes/myshoots/views/layout/app.blade.php
+++ themes/myshoots/views/layout/app.blade.php
@@ -74,7 +74,8 @@
   <script src="{{asset('/')}}jw-styles/themes/myshoots/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="{{asset('/')}}jw-styles/themes/myshoots/assets/vendor/js/lightbox-plus-jquery.min.js"></script>
     <!-- Bootstrap Date-Picker Plugin -->
-  <script type="text/javascript" src="https:/cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+  
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/> 
 <style>
     .datepicker-inline{
