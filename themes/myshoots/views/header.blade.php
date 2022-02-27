330f301612ea7717a133b30a20c47e589009fa4b
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-02-28 01:14:09 +0530

    change slider bg

--- themes/myshoots/views/header.blade.php
+++ themes/myshoots/views/header.blade.php
@@ -48,7 +48,7 @@
 
   <!-- Masthead -->
   <header class="masthead text-white text-center">
-    <div class="container-fluid p-0">
+    <div class="container-fluid p-0 bg-light">
       <div class="row no-gutters align-items-center">
         <div class="col-md-5 d-none d-md-block">
           <a href="{{url('/')}}" title="{{ get_config('title') }}"><img src="{{ upload_url(get_config('logo')) }}" title="{{ get_config('title') }}" class="w-25 img-fluid .d-sm-none .d-md-block mx-auto py-4"></a>
