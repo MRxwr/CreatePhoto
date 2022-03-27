a3adbccf481f7882c68486107c48b2409071eee2
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-03-28 00:00:36 +0530

    Update PackageThemesServiceProvider.php

--- plugins/sbhadra/packagethemes/src/Providers/PackageThemesServiceProvider.php
+++ plugins/sbhadra/packagethemes/src/Providers/PackageThemesServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace Sbhadra\Packagethemes\Providers;
 
-
-use Sbhadra\Packagethemes\Actions\MainAction;
 use Juzaweb\Support\ServiceProvider;
+use Sbhadra\Packagethemes\Actions\MainAction;
+use Illuminate\Support\Facades\Schema;
+
 
 class PackageThemesServiceProvider extends ServiceProvider
 {
