cb358ec4623177b8467dfc9aaf10372ce7347885
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-04-18 00:24:18 +0530

    added ar lang

---
+++ plugins/sbhadra/demahoption/src/resources/lang/ar/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name' => 'Name',
+    'is_theme_category' => 'theme category page enable',
+    'is_pieces_enable' => 'Pieces field enable ',
+    'rate_per_pieces' => 'Rate per pieces',
+    'theme_category' => 'Select theme categories',
+    'Number_of_Pieces' => 'Number of Pieces ',
+    'Pictures_type' => 'Pictures type',
+    'Electonic' => 'Electonic',
+    'Printed_Electonic' => 'Printed + Electonic',
+    'BookNow' => 'Book Now',
+
+];
