b42fd452223d354fd83aafec124b31f1d86861d5
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-04-18 22:31:26 +0530

    Update app.blade.php

--- themes/demah/views/layout/app.blade.php
+++ themes/demah/views/layout/app.blade.php
@@ -46,7 +46,7 @@
         @endif
 
 		<!-- responsive stylesheet -->
-        <link rel="stylesheet" href="{{asset('/')}}jw-styles/themes/demah/assets/css/responsive.css">
+        <link rel="stylesheet" href="{{asset('/')}}jw-styles/themes/demah/assets/css/responsive.css?v=101">
 
 		<!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
         <!--[if lt IE 9]>
