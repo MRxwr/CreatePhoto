64741081073263a7b268f223cb0d54e614dc690e
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-04-19 23:32:58 +0530

    Work on payment gatway

--- plugins/sbhadra/payapi/src/Http/Controllers/PayapiController.php
+++ plugins/sbhadra/payapi/src/Http/Controllers/PayapiController.php
@@ -14,36 +14,57 @@ class PayapiController extends FrontendController
     {
         $bsid=base64_encode($payment_data['booking_id']);
         
-        $params = [
-            'endpoint' => 'PaymentRequestExicute',
-            'apikey' => 'CKW-1640114323-2537',
-            'PaymentMethodId' => '1',
-            'CustomerName' => $payment_data['customer_name'],
-            'DisplayCurrencyIso' => 'KWD',
-            'MobileCountryCode' => '+965',
-            'CustomerMobile' => $payment_data['mobile_number'],
-            'CustomerEmail' => 'nasserhatab@gmail.com',
-            'InvoiceValue' =>  ($payment_data['pay_amount']?$payment_data['pay_amount']:$payment_data['booking_price']),
-            'CallBackUrl' => url('payment/success').'/?bsid='.$bsid,
-            'ErrorUrl' =>  url('payment/failed').'/?bsid='.$bsid,
-            'Language' => 'en',
-            'CustomerReference' => 'Ref 0003',
-            'CustomerAddress[Block]' => '4',
-            'CustomerAddress[Street]' => '10',
-            'CustomerAddress[HouseBuildingNo]' => '4',
-            'CustomerAddress[Address]' => 'adan',
-            'CustomerAddress[AddressInstructions]' => '3rd floor',
-            'InvoiceItems[0][ItemName]' => $payment_data['customer_name'],
-            'InvoiceItems[0][Quantity]' => '1',
-            'InvoiceItems[0][UnitPrice]' => ($payment_data['pay_amount']?$payment_data['pay_amount']:$payment_data['booking_price']),
-            'ShippingConsignee[CityName]' => 'DUBAI',
-            'ShippingConsignee[PostalCode]' => '12345',
-            'ShippingConsignee[CountryCode]' => 'AE',
-            'SourceInfo' => '',
-            'Suppliers[0][SupplierCode]' => '3',
-            'Suppliers[0][ProposedShare]' => 'null',
-            'Suppliers[0][InvoiceShare]' => '0.00'
-        ];
+        // $params = [
+        //     'endpoint' => 'PaymentRequestExicute',
+        //     'apikey' => 'CKW-1640114323-2537',
+        //     'PaymentMethodId' => '1',
+        //     'CustomerName' => $payment_data['customer_name'],
+        //     'DisplayCurrencyIso' => 'KWD',
+        //     'MobileCountryCode' => '+965',
+        //     'CustomerMobile' => $payment_data['mobile_number'],
+        //     'CustomerEmail' => 'nasserhatab@gmail.com',
+        //     'InvoiceValue' =>  ($payment_data['pay_amount']?$payment_data['pay_amount']:$payment_data['booking_price']),
+        //     'CallBackUrl' => url('payment/success').'/?bsid='.$bsid,
+        //     'ErrorUrl' =>  url('payment/failed').'/?bsid='.$bsid,
+        //     'Language' => 'en',
+        //     'CustomerReference' => 'Ref 0003',
+        //     'CustomerAddress[Block]' => '4',
+        //     'CustomerAddress[Street]' => '10',
+        //     'CustomerAddress[HouseBuildingNo]' => '4',
+        //     'CustomerAddress[Address]' => 'adan',
+        //     'CustomerAddress[AddressInstructions]' => '3rd floor',
+        //     'InvoiceItems[0][ItemName]' => $payment_data['customer_name'],
+        //     'InvoiceItems[0][Quantity]' => '1',
+        //     'InvoiceItems[0][UnitPrice]' => ($payment_data['pay_amount']?$payment_data['pay_amount']:$payment_data['booking_price']),
+        //     'ShippingConsignee[CityName]' => 'DUBAI',
+        //     'ShippingConsignee[PostalCode]' => '12345',
+        //     'ShippingConsignee[CountryCode]' => 'AE',
+        //     'SourceInfo' => '',
+        //     'Suppliers[0][SupplierCode]' => '3',
+        //     'Suppliers[0][ProposedShare]' => 'null',
+        //     'Suppliers[0][InvoiceShare]' => '0.00'
+        // ];
+        $PaymentAPIKey = 'CKW-1640114323-2537';
+        $paymentMethod=1;
+        $name = $payment_data['customer_name'];
+        $phone1 = $payment_data['mobile_number'];
+        $settingsEmail = 'contact@create-kw.com';
+        $totalPrice = ($payment_data['pay_amount']?$payment_data['pay_amount']:$payment_data['booking_price']);
+
+        $params = array(
+            "endpoint"                 => "PaymentRequestExicuteForStore",
+            "apikey"                 => "$PaymentAPIKey",
+            "PaymentMethodId"         => "$paymentMethod",
+            "CustomerName"            => "$name",
+            "DisplayCurrencyIso"    => "KWD", 
+            "MobileCountryCode"        => "+965", 
+            "CustomerMobile"        => substr($phone1,0,11),
+            "CustomerEmail"            => $settingsEmail,
+            "invoiceValue"            => $totalPrice,
+            "SourceInfo"            => '',
+            "CallBackUrl"            => url('payment/success').'/?bsid='.$bsid,
+            "ErrorUrl"                => url('payment/failed').'/?bsid='.$bsid
+            );
 
         $curl = curl_init();
         // $certificate_location = 'C:\wamp64\bin\php\php7.2.33\extras\ssl\cacert.pem';
@@ -163,6 +184,27 @@ class PayapiController extends FrontendController
             //$booking_data = Session::get("booking_data");
             //dd($booking_data);
             }
+        }if(isset($_REQUEST['OrderId'])){
+            $bsid = base64_decode($_REQUEST['bsid']);
+            $paymentId = $_REQUEST['OrderId'];
+            $orderid = $_REQUEST['OrderId'];
+            $booking = Booking::find($bsid);
+            $booking->transaction_id =  $_REQUEST['OrderId'];
+            $booking->status =  'Yes';
+            if($booking->save()){
+                $slot = $booking->timeslot->starttime .'To'. $booking->timeslot->endtime;
+                $booking_date = $booking->booking_date;
+                $rptest=["[bdate]","[time]","[orderId]"];
+                $nptext = [$booking_date,$slot,$orderid];
+                $data= array(
+                   'message'=>str_replace($rptest,$nptext,trans('sbkw::app.sussess_message')),
+                   'mobile'=>$booking->mobile_number,
+                   'code'=>'+965',
+                );
+               do_action('booking.sms.index',$data);
+               //dd($data);
+            }
+
         }
     }
 
@@ -219,9 +261,9 @@ class PayapiController extends FrontendController
                          $rptest=["[bdate]","[time]","[orderId]"];
                          $nptext = [$booking_date,$slot,$orderid];
                          $data= array(
-                            'message'=>str_replace($rptest,$nptext,trans('sbpa::app.sussess_message')),
+                            'message'=>str_replace($rptest,$nptext,trans('sbkw::app.refund_message')),
                             'mobile'=>$booking->mobile_number,
-                            'code'=>'91',
+                            'code'=>'+965',
                          );
                          do_action('booking.sms.index',$data);
                         
