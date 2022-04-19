a5b4f08cdb401015ffeaccbe3b88d98c9166b229
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-04-19 15:53:31 +0530

    issues fixed 3

--- public/jw-styles/themes/demah/assets/js/main.js
+++ public/jw-styles/themes/demah/assets/js/main.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
       responsive : {
         480 : { items : 1  }, // from zero to 480 screen width 4 items
         768 : { items : 2  }, // from 480 screen widthto 768 6 items
-        1024 : { items : 3 }  // from 768 screen width to 1024 8 items 
+        1024 : { items : 4 }  // from 768 screen width to 1024 8 items 
       }
   });
 
