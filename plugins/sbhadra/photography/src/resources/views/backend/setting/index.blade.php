421e7c8833a417876409e07d5fa1201ef5aee219
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-04-21 16:54:10 +0530

    Update index.blade.php

--- plugins/sbhadra/photography/src/resources/views/backend/setting/index.blade.php
+++ plugins/sbhadra/photography/src/resources/views/backend/setting/index.blade.php
@@ -4,10 +4,10 @@
 
 <!-- @dd(\Sbhadra\Photography\Models\Setting::where('field_value','api_key')->first()) -->
     <div class="row mb-3">
-        <div class="col-md-12">
+        <div class="col-md-8">
         <form action="{{route('admin.setting.post')}}" method="post" class="form-ajax" id="Be4MBcHP47k9METK" novalidate="novalidate">
         {!! csrf_field() !!}
-        <div class="form-group row">
+            <div class="form-group row">
                     <label class="col-form-label" for="release">Pay Api Key</label>
                     <input id="timepicker1" name="api_key" type="text" class="form-control input-small" value="{{$settings[0]['field_value']}}">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>
@@ -28,9 +28,12 @@
                     <input id="timepicker2" name="sms_sender" type="text" class="form-control input-small" value="{{$settings[3]['field_value']}}">
                     <span class="input-group-addon"><i class="fa fa-watch"></i></span>
                 </div>
+                <div class="mt-3"><button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Save </button></div>
+                </form>
             </div>
-            <div class="mt-3"><button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Save </button></div>
-        </form>
+           
+       
+
     </div>
 
    
