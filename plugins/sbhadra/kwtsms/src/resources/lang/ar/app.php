70d788463d9c516d4e8d51bc28139d2c21b54b96
Sonjoy Bhadra <sbhadra0@gmail.com>
2022-06-09 23:50:39 +0530

    change on survey message

--- plugins/sbhadra/kwtsms/src/resources/lang/ar/app.php
+++ plugins/sbhadra/kwtsms/src/resources/lang/ar/app.php
@@ -6,5 +6,6 @@ return [
     'cancel_message'=>"Your booking has been canceled for Demah Studio,Booking Id:[orderId] ",
     'refund_message'=>"Your booking has been refunded for Demah Studio,Booking Id:[orderId] ",
     'survey_message'=>"Thank for successfully completed Your booking has been canceled for Demah Studio,survey link:[link] ",
-    'survey_success_message'=>"Thank for successfully completed Your survey for ,Coupon[link] ",
+    'survey_success_message'=>"Thank for successfully completed Your survey for ,Coupon [link] ",
+    'success_survey_done'=>"Thank for successfully completed Your survey for ,Coupon [link] ",
 ];
